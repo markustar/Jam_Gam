18d2e1050a0fe639f4d699ed26ba13e0d303bd9f
markustar <marcustarian69@gmail.com>
2024-02-01 20:58:11 +0100

    AddArrowAndModifineCandlePrefab

---
+++ testgame/Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(1.5f, 0, 0);
+    }
+
+    public void ShowArrow()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HideArrow()
+    {
+        gameObject.SetActive(false);
+    }
+}
