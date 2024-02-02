5135fd2394f9f43c0ed70652e64126b6e29d5289
Paul Adrian <67833935+alcoranpaul@users.noreply.github.com>
2024-02-02 00:15:11 -0600

    Revert "Merge pull request #12 from markustar/RandomizeEnemySpawn"
    This reverts commit 71acf283206aedd5d0be345d0e295c7f8507a4da, reversing
    changes made to ff910412cb0082bf6ac8684383de4bf3c481f4e0.

--- testgame/Assets/Scripts/SpawnEmemies.cs
+++ testgame/Assets/Scripts/SpawnEmemies.cs
@@ -7,27 +7,22 @@ public class SpawnEmemies : MonoBehaviour
     public GameObject enemyPrefab;
     public LitUpCandle LitUp;
     private float timer;
-    private float min = 10f;
-    private float max = 20f;
 
     private Vector3 randomPosition;
 
-    private void Start()
-    {
-        timer = Random.Range(min, max);
-    }
+
 
     void Update()
     {
-        timer -= Time.deltaTime;
+        timer += Time.deltaTime;
 
-        if (timer <= 0)
+        if (timer >= 10)
         {
-            randomPosition = new Vector3(Random.Range(transform.position.x-1, transform.position.x+1), transform.position.y+2, Random.Range(transform.position.z-1, transform.position.z+1));
+            randomPosition = new Vector3(UnityEngine.Random.Range(transform.position.x-1, transform.position.x+1), transform.position.y+2, UnityEngine.Random.Range(transform.position.z-1, transform.position.z+1));
             
             if (LitUp.CandleIsLitUp == false)
             {
-                timer = Random.Range(min, max);
+                timer = 0f;
                 Instantiate(enemyPrefab, randomPosition, transform.rotation);
             }
         }
