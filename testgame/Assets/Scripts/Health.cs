5384b790f2bf2a1db16fb6bca9a103d8e0877060
markustar <marcustarian69@gmail.com>
2024-02-02 07:57:04 +0100

    BulletMaterialFullHealth

--- testgame/Assets/Scripts/Health.cs
+++ testgame/Assets/Scripts/Health.cs
@@ -39,7 +39,7 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        health = 10f;
+        health = 50f;
     }
 
     // Update is called once per frame
