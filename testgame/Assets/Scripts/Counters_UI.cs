6ea7032f0aa8021d708d3058e4e7a5bd6ff9bc7e
Paul Adrian <67833935+alcoranpaul@users.noreply.github.com>
2024-02-02 01:44:51 -0600

    FINAL
    - UI isntruction
    - Random Enemy Spawn
    - Counters UI
    - Health Low Warning UI
    - TWEAKS

---
+++ testgame/Assets/Scripts/Counters_UI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Counters_UI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI enemiesTxt;
+    [SerializeField] private TextMeshProUGUI candlesTxt;
+
+    private GameManager gameManager;
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+        gameManager.OnToggleEnemies += OnEnemiesToggled;
+        gameManager.OnToggleCandle += GameManager_OnLitCandles;
+        candlesTxt.text = "0";
+        enemiesTxt.text = "0";
+    }
+
+
+     private void GameManager_OnLitCandles(object sender, System.EventArgs e)
+    {
+        candlesTxt.text = gameManager.CandlesUnlit.ToString();
+    }
+
+    private void OnEnemiesToggled(object sender, System.EventArgs e)
+    {
+        enemiesTxt.text = gameManager.EnemiesAlive.ToString();
+    }
+}
